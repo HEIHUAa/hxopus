0a74d58b4b0d14ff6aec0bfbbc8f7d74278a1b66
HEIHUAa <112499486+HEIHUAa@users.noreply.github.com>
2025-09-21 17:04:44 +0800

    Adaptive CodenameEngine

--- hxopus/Opus.hx
+++ hxopus/Opus.hx
@@ -1,6 +1,5 @@
 package hxopus;
 
-import cpp.ConstCharStar;
 import haxe.io.BytesData;
 import haxe.io.Bytes;
 #if openfl
@@ -8,20 +7,12 @@ import openfl.media.Sound;
 import openfl.utils.Assets;
 import openfl.utils.ByteArray;
 #end
-#if flixel
-import flixel.sound.FlxSound;
-#end
 
 @:buildXml("<include name='${haxelib:hxopus}/build.xml' />")
 @:include("hxopus.hpp")
 extern class Opus {
-	/**
-	 * Gets the current version of libopus.
-	 * @return String
-	 */
-	public static inline function getVersionString():ConstCharStar {
-		return opus_get_version_string();
-	}
+	@:native("hxopus_to_bytes")
+	extern static function hxopus_to_bytes(encodedBytes:BytesData):BytesData;
 
 	/**
 	 * Decodes the audio data and returns it as bytes.
@@ -29,7 +20,11 @@ extern class Opus {
 	 * @return BytesData
 	 */
 	public static inline function getDecodedBytes(encodedBytes:Bytes):BytesData {
+		#if cpp
 		return hxopus_to_bytes(encodedBytes.getData());
+		#else
+		throw "Opus audio decoding is only supported on C++ targets.";
+		#end
 	}
 
 	#if openfl
@@ -59,36 +54,4 @@ extern class Opus {
 		return sound;
 	}
 	#end
-
-	#if flixel
-	/**
-	 * Returns a new `FlxSound` object.
-	 * @param bytes The raw bytes to decode.
-	 * @param looped Should the audio loop?
-	 * @param autoDestroy Should the `FlxSound` instance should destroy on state close.
-	 * @param onComplete Called when finished playing,
-	 * @return FlxSound
-	 */
-	public overload extern inline static function toFlxSound(bytes:Bytes, looped:Bool = false, autoDestroy:Bool = false, ?onComplete:Void->Void):FlxSound {
-		return new FlxSound().loadEmbedded(toOpenFL(bytes), looped, autoDestroy, onComplete);
-	}
-
-	/**
-	 * Returns a new `FlxSound` object.
-	 * @param file The name of the file to open.
-	 * @param looped Should the audio loop?
-	 * @param autoDestroy Should the `FlxSound` instance should destroy on state close.
-	 * @param onComplete Called when finished playing,
-	 * @return FlxSound
-	 */
-	public overload extern inline static function toFlxSound(file:String, looped:Bool = false, autoDestroy:Bool = false, ?onComplete:Void->Void):FlxSound {
-		return new FlxSound().loadEmbedded(toOpenFL(file), looped, autoDestroy, onComplete);
-	}
-	#end
-
-	@:native("hxopus_get_version_string")
-	private static function opus_get_version_string():ConstCharStar;
-
-	@:native("hxopus_to_bytes")
-	private static function hxopus_to_bytes(encodedBytes:BytesData):BytesData;
 }
